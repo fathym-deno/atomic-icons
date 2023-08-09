43c726e1fc68c3d56e52fe6cdbc9882cc8055397
mcgear <michael.gearhardt@gmail.com>
2023-08-09 12:00:10 -0600

    lower name

--- src/plugin/IconSet.plugin.tsx
+++ src/plugin/IconSet.plugin.tsx
@@ -4,7 +4,7 @@ import { Plugin } from "../src.deps.ts";
 import { IconSetMiddleware } from "./routes/_middleware.tsx";
 import { establishIconSetSheet } from "./routes/sprites/icon-set.sheet.tsx";
 
-export function IconSetPlugin(
+export function iconSetPlugin(
   config: IconSetGenerateConfig,
 ): Plugin {
   // await useSheetComponents(config);
