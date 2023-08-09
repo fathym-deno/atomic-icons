46e4e25b7c1f31bb404590230ccf7eda27a4b984
mcgear <michael.gearhardt@gmail.com>
2023-08-09 11:00:26 -0600

    route only logging

--- src/plugin/routes/_middleware.tsx
+++ src/plugin/routes/_middleware.tsx
@@ -3,16 +3,15 @@ import { MiddlewareHandlerContext } from "../../src.deps.ts";
 export function spriteMapMiddleware() {
   return async (_req: Request, ctx: MiddlewareHandlerContext) => {
     {
-      // if (ctx.destination != "route") {
-      //   return await ctx.next();
-      // }
+      if (ctx.destination == "route") {
+        console.log(JSON.stringify(ctx));
+        console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+      }
       // if (!Context.instance()) {
       //   await Context.init(path);
       // }
       // ctx.state.context = Context.instance();
-      console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
-      console.log(JSON.stringify(ctx));
       return await ctx.next();
     }
   };
