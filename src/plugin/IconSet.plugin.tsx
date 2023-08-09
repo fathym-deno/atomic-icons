3ae55acfb0c45bf908440f5d112dd14b5688b506
mcgear <michael.gearhardt@gmail.com>
2023-08-09 13:33:10 -0600

    no iconset components

--- src/plugin/IconSet.plugin.tsx
+++ src/plugin/IconSet.plugin.tsx
@@ -7,10 +7,10 @@ import { Plugin } from "../src.deps.ts";
 import { IconSetMiddleware } from "./routes/_middleware.tsx";
 import { establishIconSetSheet } from "./routes/sprites/icon-set.sheet.tsx";
 
-export async function iconSetPlugin(
+export function iconSetPlugin(
   config: IconSetGenerateConfig,
 ): Promise<Plugin> {
-  await useIconSetComponents(config);
+  // await useIconSetComponents(config);
 
   const iconSetSheet = establishIconSetSheet(config.Sprites!);
 
