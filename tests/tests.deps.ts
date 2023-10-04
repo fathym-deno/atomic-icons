42476103e7ae0f5d91e80bc829dca881af6f97c0
mcgear <michael.gearhardt@gmail.com>
2023-10-03 23:36:25 -0600

    cleanup refs

--- tests/tests.deps.ts
+++ tests/tests.deps.ts
@@ -3,11 +3,11 @@ export { createHandler, type ServeHandlerInfo } from "$fresh/server.ts";
 export {
   assert,
   assertEquals,
-} from "https://deno.land/std@0.197.0/testing/asserts.ts";
+} from "https://deno.land/std@0.203.0/testing/asserts.ts";
 export {
   afterEach,
   beforeEach,
   describe,
   it,
-} from "https://deno.land/std@0.197.0/testing/bdd.ts";
-export { render } from "https://esm.sh/preact-render-to-string@6.2.0";
+} from "https://deno.land/std@0.203.0/testing/bdd.ts";
+export { render } from "preact-render-to-string";
