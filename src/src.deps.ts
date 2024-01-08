a55f6b2105b9550b75dab8621a57a41b56899543
mcgear <michael.gearhardt@gmail.com>
2024-01-08 12:38:40 -0700

    upgrades

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -10,14 +10,10 @@ export type {
 export { js2xml } from "https://deno.land/x/js2xml@1.0.4/mod.ts";
 export { xml2js } from "https://deno.land/x/xml2js@1.0.0/mod.ts";
 export { render } from "preact-render-to-string";
-export {
-  dirname,
-  join,
-  resolve,
-} from "https://deno.land/std@0.203.0/path/mod.ts";
+export { dirname, join, resolve } from "$std/path/mod.ts";
 export { paramCase, pascalCase } from "https://deno.land/x/case@2.1.1/mod.ts";
 export {
   exists,
   existsSync,
-} from "https://deno.land/x/fathym_common@v0.0.100-integration/mod.ts";
+} from "https://deno.land/x/fathym_common@v0.0.120/mod.ts";
 export { optimize as optimizeSvg } from "npm:svgo";
