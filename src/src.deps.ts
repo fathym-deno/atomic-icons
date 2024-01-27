3727c8e728d01d4e7f4cd0f82254f729f3a96360
mcgear <michael.gearhardt@gmail.com>
2024-01-26 20:47:53 -0700

    ref updates

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -15,5 +15,5 @@ export { paramCase, pascalCase } from "https://deno.land/x/case@2.1.1/mod.ts";
 export {
   exists,
   existsSync,
-} from "https://deno.land/x/fathym_common@v0.0.134/mod.ts";
+} from "https://deno.land/x/fathym_common@v0.0.137/mod.ts";
 export { optimize as optimizeSvg } from "npm:svgo";
