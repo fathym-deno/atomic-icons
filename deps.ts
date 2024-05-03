03e9e7b83f2e585bcd4a4227c482b57b60d250b1
mcgear <michael.gearhardt@gmail.com>
2024-05-03 09:19:34 -0600

    ref updates

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
 export { type JSX } from "https://esm.sh/preact@10.20.1";
-export * from "https://deno.land/x/fathym_ioc@v0.0.7/mod.ts";
+export * from "https://deno.land/x/fathym_ioc@v0.0.10/mod.ts";
