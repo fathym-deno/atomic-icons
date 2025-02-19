b09bbc81763a88ad10393f1ffe82d97cdcec66cb
mcgear <michael.gearhardt@gmail.com>
2025-02-18 17:29:29 -0700

    ref updates

--- src/deno.deps.ts
+++ src/deno.deps.ts
@@ -1,16 +1,15 @@
-export { exists, existsSync } from "jsr:@fathym/common@0.2.175/path";
+export { exists, existsSync } from "jsr:@fathym/common@0.2.178/path";
 
-export { type EaCRuntimeConfig } from "jsr:@fathym/eac@0.2.36/runtime/config";
-export { type EaCRuntimeHandler } from "jsr:@fathym/eac@0.2.36/runtime/pipelines";
 export {
-  type EaCRuntimePlugin,
+  type EaCRuntimeConfig,
   type EaCRuntimePluginConfig,
-} from "jsr:@fathym/eac@0.2.36/runtime/plugins";
+} from "jsr:@fathym/eac@0.2.78/runtime/config";
+export { type EaCRuntimeHandler } from "jsr:@fathym/eac@0.2.78/runtime/pipelines";
+export { type EaCRuntimePlugin } from "jsr:@fathym/eac@0.2.78/runtime/plugins";
 
 export {
   type EaCApplicationProcessorConfig,
   type EaCProcessor,
   isEaCProcessor,
-} from "jsr:@fathym/eac-applications@0.0.54/processors";
-
-export { type ProcessorHandlerResolver } from "jsr:@fathym/eac-applications@0.0.54/runtime/processors";
+} from "jsr:@fathym/eac-applications@0.0.78/processors";
+export { type ProcessorHandlerResolver } from "jsr:@fathym/eac-applications@0.0.78/runtime/processors";
--- src/src.deps.ts
+++ src/src.deps.ts
@@ -1,19 +1,19 @@
 export * from "../deps.ts";
 
-export * as path from "jsr:@std/path@1.0.2";
-export * from "jsr:@std/http@1.0.3/status";
+export * as path from "jsr:@std/path@1.0.8";
+export * from "jsr:@std/http@1.0.13/status";
 
-export { getPackageLogger } from "jsr:@fathym/common@0.2.175/log";
+export { getPackageLogger } from "jsr:@fathym/common@0.2.178/log";
 
 export {
   parse as parseXml,
   stringify as buildXml,
   type xml_document,
   type xml_node,
-} from "jsr:@libs/xml@5.4.13";
+} from "jsr:@libs/xml@5.4.16";
 
 export { kebabCase, pascalCase } from "jsr:@luca/cases@1.0.0";
 
-export { render as preactRenderToString } from "npm:preact-render-to-string@6.3.1";
+export { render as preactRenderToString } from "npm:preact-render-to-string@6.5.13";
 
 export { optimize as optimizeSvg } from "npm:svgo@3.3.2";
